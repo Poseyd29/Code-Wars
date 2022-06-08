630a4069ac69ae29db5173a3458fc6a36764c333
Poseyd29 <poseyd29@gmail.com>
2022-06-08 16:50:05 -0400

    Code Wars Solution
    Code Wars Solution

---
+++ Beginner-Series-4-Cockroach/main.js
@@ -0,0 +1,10 @@
+function cockroachSpeed(s) {
+    let centimeter = s
+    const kiloConverted = centimeter * 100000
+    const kiloPerMinute = kiloConverted / 60
+    const cps = kiloPerMinute / 60
+
+    return Math.floor(cps)
+
+
+}
